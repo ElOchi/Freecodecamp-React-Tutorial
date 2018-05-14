1810dd8e716af846acd177b19e259bc876f89dbd
dante <dantezell78@gmail.com>
2018-05-13 22:53:25 -0400

    add navbar

--- Freecode-tutorial/src/App.js
+++ Freecode-tutorial/src/App.js
@@ -1,21 +1,99 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
-class App extends Component {
+class Login extends Component {
+  static muiName = 'FlatButton';
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ochi's React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return ( <
+      FlatButton { ...this.props
+      }
+      label = "Login" / >
     );
   }
 }
 
-export default App;
+const Logged = (props) => ( <
+  IconMenu { ...props
+  }
+  iconButtonElement = { <
+    IconButton > < MoreVertIcon / > < /IconButton>
+  }
+  targetOrigin = {
+    {
+      horizontal: 'right',
+      vertical: 'top'
+    }
+  }
+  anchorOrigin = {
+    {
+      horizontal: 'right',
+      vertical: 'top'
+    }
+  } >
+  <
+  MenuItem primaryText = "Refresh" / >
+  <
+  MenuItem primaryText = "Help" / >
+  <
+  MenuItem primaryText = "Sign out" / >
+  <
+  /IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+/**
+ * This example is taking advantage of the composability of the `AppBar`
+ * to render different components depending on the application state.
+ */
+class AppBarExampleComposition extends Component {
+  state = {
+    logged: true,
+  };
+
+  handleChange = (event, logged) => {
+    this.setState({
+      logged: logged
+    });
+  };
+
+  render() {
+    return ( <
+      div >
+      <
+      Toggle label = "Logged"
+      defaultToggled = {
+        true
+      }
+      onToggle = {
+        this.handleChange
+      }
+      labelPosition = "right"
+      style = {
+        {
+          margin: 20
+        }
+      }
+      /> <
+      AppBar title = "Title"
+      iconElementLeft = { < IconButton > < NavigationClose / > < /IconButton>}
+        iconElementRight = {
+          this.state.logged ? < Logged / > : < Login / >
+        }
+        /> <
+        /div>
+      );
+    }
+  }
+
+  export default AppBarExampleComposition;
